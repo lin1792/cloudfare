45667bb9dd3803a52b345f674511f912cbdedda7
lin1792 <1792048892@qq.com>
2024-04-14 12:22:50 +0800

    init

--- server/api/ceshi.get.ts
+++ server/api/ceshi.get.ts
@@ -3,14 +3,14 @@ import { eq } from 'drizzle-orm'
 export default eventHandler(async (event) => {
 
   // const session = await requireUserSession(event)
-  // const todo = await useDB().insert(tables.todos).values({
-  //   userId: 1,
-  //   title:'ceshitable',
-  //   createdAt: new Date()
-  // }).returning().get()
+  const todo = await useDB().insert(tables.todos).values({
+    userId: 1,
+    title:'ceshitable',
+    createdAt: new Date()
+  }).returning().get()
 
   // List todos for the current user
-  const todo = await useDB().select().from(tables.todos).all()
+  // const todo = await useDB().select().from(tables.todos).all()
 
   return todo
 })
