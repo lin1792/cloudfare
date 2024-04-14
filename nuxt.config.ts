b8615c319acd820d1544fd2ce069f4ee20142c35
lin1792 <1792048892@qq.com>
2024-04-14 11:27:26 +0800

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxthub/core","nuxt-auth-utils"],
+  modules: ["@nuxthub/core", "nuxt-auth-utils"],
+  // $development: {
+  //   hub: {
+  //     remote: true
+  //   }
+  // },
   hub: {
     database: true,
+    remote: true
   },
   css: [
     'element-plus/dist/index.css',
