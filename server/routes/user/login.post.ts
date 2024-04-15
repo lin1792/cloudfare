36e03d85a6ba6f06180e9f4c6841db62d3edbdb1
lin1792 <1792048892@qq.com>
2024-04-15 23:59:31 +0800

    添加token登录

--- server/routes/user/login.post.ts
+++ server/routes/user/login.post.ts
@@ -12,29 +12,30 @@ export default eventHandler( async (event) => {
 
   // 在这里进行用户名和密码的验证逻辑
   // await useDB().select().from(tables.todos).where(eq(tables.todos.userId, session.user.id)).all()
-  const nameMatch = await useDB().select().from(tables.user).where(eq(tables.user.userName, userName))
-  console.log(nameMatch);
-  if (nameMatch.length<1) {
-    return {code:401,data:null,message:'用户名不存在'}
-  }
-  // const passwordMatch = await useDB().select(tables.user.password).from(tables.user).where(eq(tables.user.userName, userName))
-  if (nameMatch[0].password != password) {
-    return {code:401,data:null,message:'密码错误'}
-  }
+  // const nameMatch = await useDB().select().from(tables.user).where(eq(tables.user.userName, userName))
+  // console.log(nameMatch);
+  // if (nameMatch.length<1) {
+  //   return {code:401,data:null,message:'用户名不存在'}
+  // }
+  // // const passwordMatch = await useDB().select(tables.user.password).from(tables.user).where(eq(tables.user.userName, userName))
+  // if (nameMatch[0].password != password) {
+  //   return {code:401,data:null,message:'密码错误'}
+  // }
   
-  // 如果验证通过，生成一个包含用户信息的 payload
-  const payload = { id: nameMatch[0].id };
+  // // 如果验证通过，生成一个包含用户信息的 payload
+  // const payload = { id: nameMatch[0].id };
+  const payload = '123';
 
   // 生成 JWT 令牌
   const token = generateToken(payload);
 
   // // 返回包含令牌的响应
-  // // res.json({ token });
-  // await setUserSession(event, {
-  //   user: {
-  //     token:token
-  //   },
-  //   // Any extra fields
-  // })
+  // res.json({ token });
+  await setUserSession(event, {
+    user: {
+      token:token
+    },
+    // Any extra fields
+  })
   return {code:200,data:token,message:'登录成功'}
  })
