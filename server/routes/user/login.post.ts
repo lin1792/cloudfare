59ffe1d79ab8c2d49a789e1f47d6c8748d96bbd4
lin1792 <1792048892@qq.com>
2024-04-16 00:08:11 +0800

    添加token登录

--- server/routes/user/login.post.ts
+++ server/routes/user/login.post.ts
@@ -1,30 +1,30 @@
 //登录接口
-import { eq } from 'drizzle-orm'
+// import { eq } from 'drizzle-orm'
 import { generateToken } from "../../utils/auth";
 export default eventHandler( async (event) => {
 
-  const body = await readBody(event);
-  console.log('123', body);
+  // const body = await readBody(event);
+  // console.log('123', body);
 
 
-  // const { userName, password } = body;
+  // // const { userName, password } = body;
 
 
-  // 在这里进行用户名和密码的验证逻辑
-  // await useDB().select().from(tables.todos).where(eq(tables.todos.userId, session.user.id)).all()
-  const nameMatch = await useDB().select().from(tables.user).where(eq(tables.user.userName, body.userName)).all()
-  console.log(nameMatch);
-  if (nameMatch.length<1) {
-    return {code:401,data:null,message:'用户名不存在'}
-  }
-  // const passwordMatch = await useDB().select(tables.user.password).from(tables.user).where(eq(tables.user.userName, userName))
-  if (nameMatch[0].password != body.password) {
-    return {code:401,data:null,message:'密码错误'}
-  }
+  // // 在这里进行用户名和密码的验证逻辑
+  // // await useDB().select().from(tables.todos).where(eq(tables.todos.userId, session.user.id)).all()
+  // const nameMatch = await useDB().select().from(tables.user).where(eq(tables.user.userName, body.userName)).all()
+  // console.log(nameMatch);
+  // if (nameMatch.length<1) {
+  //   return {code:401,data:null,message:'用户名不存在'}
+  // }
+  // // const passwordMatch = await useDB().select(tables.user.password).from(tables.user).where(eq(tables.user.userName, userName))
+  // if (nameMatch[0].password != body.password) {
+  //   return {code:401,data:null,message:'密码错误'}
+  // }
   
-  // 如果验证通过，生成一个包含用户信息的 payload
-  const payload = { id: nameMatch[0].id };
-  // const payload = ;
+  // // 如果验证通过，生成一个包含用户信息的 payload
+  // const payload = { id: nameMatch[0].id };
+  const payload = '123';
 
   // 生成 JWT 令牌
   const token = generateToken(payload);
