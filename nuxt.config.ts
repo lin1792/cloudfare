09e2ca0f4d3f74615eb6b6a07858c576974caff2
linbinhui <1792048892@qq.com>
2024-04-16 15:08:48 +0800

    解决冲突

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// import { nodePolyfills } from 'vite-plugin-node-polyfills'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   build: {
     transpile: ['jsonwebtoken']
   },
+  // plugins: [
+  //   nodePolyfills(),
+  // ],
   modules: ["@nuxthub/core", "nuxt-auth-utils",'@nuxtjs/stylelint-module'],
   $development: {
     hub: {
