d446ee8e2e8ca2f9e4608889bafc9c7e67dabd28
linbinhui <1792048892@qq.com>
2024-04-16 15:18:41 +0800

    解决冲突

--- server/routes/user/login.post.ts
+++ server/routes/user/login.post.ts
@@ -24,7 +24,7 @@ export default eventHandler( async (event) => {
   // const token =  jwt.sign(payload, secretKey,{
   //   expiresIn: (60 * 60 * 24) * 7//7天有效期
   // });
-  const a=typeof(jwt.sign)
+  const a=typeof(jwt)
   // // 返回包含令牌的响应
   // res.json({ token });
   await setUserSession(event, {
